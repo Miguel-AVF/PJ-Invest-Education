cf030e99dffe8503c7ee117289be154f54ab20d8
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-05-30 23:26:26 -0300

    Pagina Inicial

--- index.php
+++ index.php
@@ -1,3 +1,86 @@
-<p>O <b>Dusk Invest Education</b> tem como objetivo apresentar conceitos fundamentais de educação financeira e ensinar, de forma acessível, como iniciar no mundo dos investimentos. Desde o planejamento financeiro
+<?php
+
+include __DIR__. '/includes/head.php';
+
+?>
+
+<header class="main-header">
+        <div class="logo">DUSK <span>INVEST EDUCATION</span></div>
+        <nav class="nav-menu">
+            <a href="#">Inicio</a>
+            <a href="#">Informativo</a>
+            <a href="#">Simulador</a>
+            <a href="#">Sobre Nós</a>
+            <a href="#">Carteira</a>
+        </nav>
+
+        <div class="user-profile">
+            <img src="image/pfp.avif" alt="Foto de Perfil">
+        </div>
+    </header>
+
+    <!-- BANNER PRINCIPAL (HERO) -->
+    <section class="hero-banner">
+        <div class="hero-content">
+            <h1>Dusk Invest Education</h1>
+            <p>O <b>Dusk Invest Education</b> tem como objetivo apresentar conceitos fundamentais de educação financeira e ensinar, de forma acessível, como iniciar no mundo dos investimentos. Desde o planejamento financeiro
      pessoal até a análise de diferentes tipos de investimentos, como renda fixa e variável. Elaboração de um guia prático voltado para iniciantes. Como resultado, espera-se
       contribuir para a conscientização financeira e auxiliar indivíduos na tomada de decisões mais seguras e eficientes.</p>
+        </div>
+    </section>
+
+    <!-- SEÇÃO DE ESTOQUE DESTAQUE -->
+    <section id="estoque" class="stock-section">
+        <div class="section-title">
+            <h2>Funções</h2>
+            <p>Veículos exclusivos selecionados a dedo</p>
+        </div>
+
+        <div class="stock-grid">
+            <!-- Card de Carro 1 -->
+            <div class="car-card">
+                <div class="car-image">
+                    <img src="image/grafico.avif" alt="Grafico">
+                    
+                </div>
+                <div class="car-info">
+                    <h3 class="h3">Graficos</h3>
+                    <p class="car-details">qualquer coisa</p>
+                    <a href="" class="btn-card-wa" target="">
+                        <i class=""></i> Veja Agora
+                    </a>
+                </div>
+            </div>
+
+            <!-- Card de Carro 2 -->
+            <div class="car-card">
+                <div class="car-image">
+                    <img src="image/inform.jpeg" alt="Informação">
+                    
+                </div>
+                <div class="car-info">
+                    <h3 class="h3">Informações</h3>
+                    <p class="car-details">Ai ai ai Carrapato não tem pai</p>
+                    <a href="" class="btn-card-wa disabled">Veja Agora</a>
+                </div>
+            </div>
+
+            <!-- Card de Carro 3 -->
+            <div class="car-card">
+                <div class="car-image">
+                    <img src="image/simula.png" alt="Simulação">
+                    
+                </div>
+                <div class="car-info">
+                    <h3 class="h3">Simulações</h3>
+                    <p class="car-details">Blá blá blá</p>
+
+                    <a href="" class="btn-card-wa" target="">
+                        <i class="fab fa-whatsapp"></i> Veja Agora    
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
