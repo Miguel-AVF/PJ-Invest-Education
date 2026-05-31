f42664523fd06522a1a60064cb5f9b13fcf3d0e2
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-05-30 23:22:17 -0300

    Link CSS e Icone

--- includes/head.php
+++ includes/head.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="icon" type="image/png" href="image/icone-dusk.jpeg">
     <title>Dusk Invest Education</title>
 </head>
 <body>
