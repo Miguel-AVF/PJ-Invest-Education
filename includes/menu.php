c40381018a8ef192222e50e996a3a9d3d5d9b1c9
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-06-08 22:58:22 -0300

    Menu Padronizado para Todas as Paginas

---
+++ includes/menu.php
@@ -0,0 +1,21 @@
+ <header class="main-header">
+        <div class="logo">DUSK <span>INVEST EDUCATION</span></div>
+        <nav class="nav-menu">
+            <a href="index.php">Inicio</a>
+            <a href="informa.php">Informativo</a>
+            <a href="">Simulador</a>
+            <a href="sobre.php">Sobre Nós</a> 
+            <a href="">Carteira</a>
+        </nav>
+
+        <div class="search-bar">
+            <form action="/buscar" method="GET">
+                <input type="text" placeholder="Buscar no site..." name="q">
+                <button type="submit"><i class="fas fa-search"></i>🔎</button>
+            </form>
+        </div>
+
+        <div class="user-profile">
+            <img src="image/pfp.avif" alt="Foto de Perfil">
+        </div>
+    </header>
